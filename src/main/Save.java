50c300962fd882781a2938ffbdc37520045c4d3b
JoseFrancisR <josefrancisromanillos@gmail.com>
2026-06-30 22:06:16 +0800

    Completed some functions on the save class

--- src/main/Save.java
+++ src/main/Save.java
@@ -1,5 +1,7 @@
 package src.main;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -15,7 +17,20 @@ public class Save {
      * @return true when the corresponding file exists
      */
     public boolean saveExists(String name) {
-        // code here
+
+        File curDirectory = new File(".").getAbsoluteFile();
+        File targetFile = new File(curDirectory.getParentFile(), "data/saves/" + name);
+
+        try {
+            File actuaFile = targetFile.getCanonicalFile;
+            if (actualFile.isFile() && actualFile.exists) {
+                System.out.println("Save exists");
+                return true;
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("ERROR: in finding the file due to " + e.getMessage());
+        }
+        System.out.println("Save doesn't exists");
         return false;
     }
 
@@ -36,8 +51,25 @@ public class Save {
      * @param name player/save name
      * @return loaded player, or null when loading fails
      */
-    public Player loadPlayer(String name) {
-        // code here
+    public Player loadPlayer(Scanner scanner, String name) {
+        File curDirectory = new File(".").getAbsoluteFile();
+        File targetFile = new File(curDirectory.getParentFile(), "data/saves/" + name);
+        String playerName;
+        int playerCrystal;
+        Inventory = new Inventory();
+        RecipeBook recipe = new RecipeBook();
+
+        try {
+            File actuaFile = targetFile.getCanonicalFile;
+            if (actualFile.isFile() && actualFile.exists) {
+                while (scanner.hasNextLine()) {
+
+                }
+                return player();
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("ERROR: in finding the file due to " + e.getMessage());
+        }
         return null;
     }
 
@@ -47,19 +79,47 @@ public class Save {
      * @param player player to save
      * @return true when saving succeeds
      */
-    public boolean savePlayer(Player player) {
-        // code here
+    public boolean savePlayer(Scanner scanner, Player player) {
+        boolean inputCheck = true;
+        int input;
+        File curDirectory = new File(".").getAbsoluteFile();
+        File targetFile = new File(curDirectory.getParentFile(), "data/saves/" + name);
+        try {
+            File actuaFile = targetFile.getCanonicalFile;
+
+            if (saveExists(player.getName())) {
+                do {
+                    inputCheck = true;
+                    System.out.println("Do you want to overwrite the save(0-NO / 1-YES)");
+                    input = scanner.nextInt();
+                    if (input != 0 || input != 1) {
+                        System.out.println("INPUT ONLY 1 or 0");
+                        inputCheck = false;
+                    }
+                } while (inputCheck);
+                PrintWriter saveFile = new PrintWriter(player.getName());
+            }
+            try (PrintWriter saveFile = new PrintWriter(player.getName())) {
+                saveFile.println("NAME = " + player.getName() + "\n");
+                saveFile.println("CRYSTALS = " + player.getCrystals() + "\n");
+                // writeInventory(PrintWriter writer, Inventory inventory) NOT DONEEE
+                // void writeRecipebook(PrintWriter writer, RecipeBook recipeBook
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("ERROR: in writing the file due to " + e.getMessage());
+        }
         return false;
     }
 
     /**
      * Reads ingredient and cauldron values under the INVENTORY section.
      *
-     * @param scanner save-file scanner
+     * @param scanner   save-file scanner
      * @param inventory inventory being populated
      */
     private void readInventory(Scanner scanner, Inventory inventory) {
-        // code here
+
     }
 
     /**
@@ -69,14 +129,13 @@ public class Save {
      * @return unlocked recipe IDs
      */
     private ArrayList<Integer> readUnlockedRecipeIds(Scanner scanner) {
-        // code here
         return new ArrayList<>();
     }
 
     /**
      * Writes ingredient quantities and cauldron counts.
      *
-     * @param writer save-file writer
+     * @param writer    save-file writer
      * @param inventory player inventory
      */
     private void writeInventory(PrintWriter writer, Inventory inventory) {
@@ -86,7 +145,7 @@ public class Save {
     /**
      * Writes comma-separated unlocked recipe IDs.
      *
-     * @param writer save-file writer
+     * @param writer     save-file writer
      * @param recipebook player recipebook
      */
     private void writeRecipebook(PrintWriter writer, RecipeBook recipeBook) {
