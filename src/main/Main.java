f995fbcccf0ebdd7f72f66ca08f35f5a0a5bebf7
JoseFrancisR <josefrancisromanillos@gmail.com>
2026-07-03 00:27:20 +0800

    Add main.java file

---
+++ src/main/Main.java
@@ -0,0 +1,11 @@
+package src.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MainMenu menu = new MainMenu();
+		menu.start();
+	}
+
+}
